142277862738d92c46d9dd872968d39f31f033d6
luo <t01ql23@abdn.ac.uk>
2024-07-24 00:23:10 +0100

    add

--- djangoBackend/settings.py
+++ djangoBackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['sbx-hotel-srv1.abdn.ac.uk', '*', 'localhost']
 
-CSRF_TRUSTED_ORIGINS = ['https://localhost', 'https://127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://localhost', '*']
 
 # Application definition
 
